683981e4514a3478f67a3b6db9013c0f21c91afd
cedric e goddard <8ceg6@comcast.net>
2019-07-16 05:21:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // add solution here
+function theBeatlesPlay(musicians,insruments){
+          var array=[];
+          for (var i =0; i< musicians.length; i++){
+          array.push(musicians[i] + ' plays '+ insruments[i]);
+          }
+          return array;
+}
+
+function johnLennonFacts(facts){
+          
+          var results =[];
+          var count = 0;
+          while (results.length < facts.length);{
+
+results.push(facts[count] + '!!!');
+            count++;
+          }
+return results;
+}
